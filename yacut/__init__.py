f7880e9c5b7098fe15058300d395591f1e816d09
Zorik Karapetyan <zorikkarapetan@gmail.com>
2023-04-24 23:00:12 +0300

    configuration

---
+++ yacut/__init__.py
@@ -0,0 +1,15 @@
+import os
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+class Config(object):
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
